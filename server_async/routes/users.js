7472ded2af00a7906c8083b503c6b9d5164e2ab8
ShahzaibSE <shahzaibanalyst@outlook.com>
2018-06-21 19:17:56 +0500

    Setting auth to a route

--- server_async/routes/users.js
+++ server_async/routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 // Controllers.
 var userController = require('./../controllers/users');
+// Security guard.
+const authRequest = require('./../middlewares/authRequest');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,7 +13,7 @@ router.get('/', function(req, res, next) {
 
 // router.get('/user', userController.user_CRUD.selectall);
 // router.get('/user', userController.selectall);
-router.get('/user', userController.selectall);
+router.get('/user', authRequest.authenticate,userController.selectall);  // Guarded request. 
 router.post('/user', userController.add);
 router.put('/user/:userid', userController.update);
 router.delete('/user', userController.delete);
--- server_async/server.js
+++ server_async/server.js
@@ -4,5 +4,7 @@ var app = require('./app');
 // Server config.
 var port = process.env.PORT| 3000;
 var server = app.listen(port, ()=> {
+    console.log(`      --        `);
+    console.log(`^ ------------- ^`);
     console.log(`Listening on port : ${server.address().port}`);
 });
